679daf2f9df91df6a2440efbac031d1435790227
Hemant8t <hemant8tidke@gmail.com>
2025-08-05 16:12:55 +0530

    Update DbConfig.js
    credentials

--- app-tier/DbConfig.js
+++ app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : 'database-1-instance-1.cbkuaikmirts.eu-west-1.rds.amazonaws.com',
+    DB_HOST : '',
     DB_USER : 'admin',
-    DB_PWD : 'Sachin123',
+    DB_PWD : '',
     DB_DATABASE : 'webappdb'
 });
